60e79f7cb2b30053fc8baea7d2397bc1d86d8abe
17110220 <43233472+17110220@users.noreply.github.com>
2020-07-17 16:40:37 +0700

    commit

--- Final_project_04/Final_project_04/DTO/BillDetail.cs
+++ Final_project_04/Final_project_04/DTO/BillDetail.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace Final_project_04.DTO
 {
     class BillDetail
     {
+        public BillDetail(string id, string billID, string foodID, int count)
+        {
+            this.ID = id;
+            this.BillID = billID;
+            this.FoodID = foodID;
+            this.Count = count;
+        }
+
+        public BillDetail(DataRow row)
+        {
+            this.ID = row["id"].ToString();
+            this.BillID = row["idbill"].ToString();
+            this.FoodID = row["idfood"].ToString();
+            this.Count = (int)row["count"];
+        }
+
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        private string foodID;
+
+        public string FoodID
+        {
+            get { return foodID; }
+            set { foodID = value; }
+        }
+
+        private string billID;
+
+        public string BillID
+        {
+            get { return billID; }
+            set { billID = value; }
+        }
+
+        private string iD;
+
+        public string ID
+        {
+            get { return iD; }
+            set { iD = value; }
+        }
+
     }
 }
